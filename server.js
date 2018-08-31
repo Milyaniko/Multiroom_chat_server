bcaa5fb855541014946e3cd92fab6bbed5ce8049
Nikolay Milya <Milyaniko@gmail.com>
2018-09-01 02:22:02 +0300

    Server is initialized

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 'use strict';
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), () => console.log(`An application is running on port ${app.get('port')}...`));
+
