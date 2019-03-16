9690400c7dc46dc3e4fab1b91347fe5e132b999d
Nikolay Milya <Milyaniko@gmail.com>
2019-03-16 22:54:21 +0300

    Chat room data model is added

---
+++ app/models/chatRoom.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = (roomId, roomName, users) => {
+    this.roomId = roomId;
+    this.roomName = roomName;
+    this.users = users;
+};
---
+++ app/models/index.js
@@ -0,0 +1,6 @@
+'use strict'
+const chatRoomModel = require('./chatRoom');
+
+module.exports = {
+    chatRoomModel,
+};
