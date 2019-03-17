64b15b845f0e29a057369194e6dcb891073ee560
Nikolay Milya <Milyaniko@gmail.com>
2019-03-17 19:37:28 +0300

    Redis has been added

--- app/config/index.js
+++ app/config/index.js
@@ -1,16 +1,23 @@
 'use strict';
 if(process.env.NODE_ENV === 'production') {
+    const redisURI = require('url').parse(process.env.REDIS_URL);
+    const redisPassword = redisURI.auth.split(':')[1];
     module.exports = {
         port: process.env.PORT || 3000,
         host: process.env.HOST || "",
         dbURI: process.env.DB_URI,
         sessionSecret: process.env.SECRET,
         fb: {
-        "clientID": process.env.fbClientID,
-        "clientSecret": process.env.fbClientSecret,
-        "callbackURL": process.env.localhost + "/auth/facebook/callback",
-        "profileFields": ["id", "displayName", "photos"]
+            "clientID": process.env.fbClientID,
+            "clientSecret": process.env.fbClientSecret,
+            "callbackURL": process.env.localhost + "/auth/facebook/callback",
+            "profileFields": ["id", "displayName", "photos"]
         },
+        redis: {
+            host: redisURI.host,
+            port: redisURI.port,
+            password: redisPassword
+        }
     }
 } else {
     module.exports = require('./development.json')
