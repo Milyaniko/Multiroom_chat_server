11c37f2a0fd6114ef7735e87ee822344d7bc00aa
Nikolay Milya <Milyaniko@gmail.com>
2019-03-27 00:42:50 +0300

    SocketIO service - logic of loading messages and rooms from database

--- app/services/socketIO.js
+++ app/services/socketIO.js
@@ -1,22 +1,101 @@
 'use strict'
-const { chatRoomModel } = require('../models');
-const { randomHex, isRoomRegistered } = require('../helpers')
+const { ChatRoom, Message } = require('../models');
+const { 
+    randomHex,
+    findRoomByName,
+    findRoomById,
+    addUserToRoom,
+    removeUserFromRoom, 
+} = require('../helpers')
+const { logger } = require('../services');
 
 module.exports = (io, app) => {
     app.locals.chatrooms = [];
+    app.locals.messages = [];
+    app.locals.typingUsers = [];
     const allRooms = app.locals.chatrooms;
+    const typingUsers = app.locals.typingUsers;
+    const messages = app.locals.messages;
     
     io.of('/roomslist').on('connection', (socket) => {
         socket.on('getChatRooms', () => {
-            socket.emit('chatRoomsList', JSON.stringify(allRooms))
-        })
+            ChatRoom.find({}, (err, chatRooms) => {
+                if (err) {
+                    logger.log('error', err);
+                } else {
+                    if (allRooms.length === 0) {
+                        allRooms.push(...chatRooms);
+                    } else {
+                        chatRooms.forEach((chatRoom) => {
+                           if (!allRooms.some(element => element.roomID === chatRoom.roomID)) {
+                               allRooms.push(chatRoom);
+                           }});
+                      }
+                     }
+                    socket.emit('chatRoomsList', JSON.stringify(allRooms))
+                    socket.broadcast.emit('chatRoomsList', JSON.stringify(allRooms))
+            });
+        });
         socket.on('createNewRoom', (newRoomInput) => {
-            if (!isRoomRegistered(allRooms, newRoomInput)) {
+            const registeredRoom = findRoomByName(allRooms, newRoomInput);
+            if (!registeredRoom) {
                 const newRoomId = randomHex();
-                allRooms.push(chatRoomModel(newRoomInput, newRoomId, []))
-                socket.emit('chatRoomsList', JSON.stringify(allRooms));
-                socket.broadcast.emit('chatRoomsList', JSON.stringify(allRooms));
+                const newChatRoom = new ChatRoom({
+                    roomName: newRoomInput,
+                    roomID: newRoomId,
+                    users: [],
+                });
+                newChatRoom.save(err => {
+                    if (err) {
+                        logger.log('error', err);
+                    } else {
+                        allRooms.push(newChatRoom);
+                        socket.emit('chatRoomsList', JSON.stringify(allRooms));
+                        socket.broadcast.emit('chatRoomsList', JSON.stringify(allRooms)); 
+                }});
             }
         });
-    })
+    });
+
+    io.of('/chat/:id').on('connection', (socket) => {
+        socket.on('join', data => {
+            Message.find({ roomID: data.roomID }, (err, roomMessages) => {
+                if(err) {
+                    logger.log('error', err);
+                } else {
+                    messages.push(...roomMessages);
+                    socket.emit('updateMessagesList', JSON.stringify(messages));
+                }
+            });
+            const usersList = addUserToRoom(allRooms, data, socket);
+            socket.broadcast.to(data.roomID).emit('updateUsersList', JSON.stringify(usersList.users));
+            socket.emit('updateUsersList', JSON.stringify(usersList.users));
+        });
+        socket.on('disconnect', () => {
+            const room = removeUserFromRoom(allRooms, socket);
+            if (room) {
+                socket.broadcast.to(room.roomID).emit('updateUsersList', JSON.stringify(room.users));
+            }
+        });
+        socket.on('newMessage', (data) => {
+            const parsedData = JSON.parse(data);
+            const newMessage = new Message(parsedData);
+            newMessage.save((err) => {
+                if(err) {
+                    logger.log('error', err);
+                } else {
+                    messages[parsedData.roomID] = newMessage;
+                    socket.broadcast.to(parsedData.roomID).emit('inMessage', JSON.stringify(newMessage));
+                }
+            });
+        });
+        socket.on('startType', (userName, roomID) => {
+            typingUsers[userName] = roomID;
+            io.emit('userTypingUpdate', JSON.stringify(typingUsers), roomID);
+        });
+        socket.on('stopType', (userName) => {
+            delete typingUsers[userName];
+            io.emit('userTypingUpdate', JSON.stringify(typingUsers));
+        });
+    });
 };
